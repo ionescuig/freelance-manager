18844f4e109aa7ed0fbee50e8491b78fd5b22ed6
Gabriel <gabriel.ionescu@customore.co>
2020-01-14 21:07:17 +0000

    Development stage: prints what settings are in use.

--- src/manager/settings/__init__.py
+++ src/manager/settings/__init__.py
@@ -2,5 +2,8 @@ from .base import *
 
 try:
     from .local import *
-except:
+    print('>>> Loaded local settings')
+except Exception as e:
+    print('>>> Error in local settings:', e)
     from .production import *
+    print('>>> Loaded production settings')
