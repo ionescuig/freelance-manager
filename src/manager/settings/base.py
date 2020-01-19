1417521d02eaf7e45dbbc2df4bb70c4ffd0f8a9e
Gabriel Ionescu <ionescuig@yahoo.com>
2020-01-19 19:24:54 +0000

    Created new app: subscriptions

--- src/manager/settings/base.py
+++ src/manager/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     # local apps
     'customers.apps.CustomersConfig',
     'passwords.apps.PasswordsConfig',
+    'subscriptions.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [
---
+++ src/subscriptions/models.py
@@ -0,0 +1,41 @@
+from datetime import date, timedelta
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+from django.db import models
+
+from customers.models import Customer
+from passwords.models import Password
+
+
+websites = [
+    ('123reg', '123Reg'),
+    ('Heroku', 'Heroku'),
+    ('GitHub', 'GitHub'),
+    ('DigitalOcean', 'DigitalOcean'),
+    ('Other', 'Other')
+]
+
+
+def date_renewal_default():
+    return date.today()+timedelta(days=365)
+
+
+class Subscription(models.Model):
+    customer     = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    password     = models.OneToOneField(Password, on_delete=models.CASCADE)
+
+    website      = models.CharField(choices=websites, default='GitHub', max_length=25)
+    date_created = models.DateField(blank=True, null=True)
+    date_renewal = models.DateField(blank=True, null=True, default=date_renewal_default)
+
+    notes        = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.customer.name + ' - ' + self.website
+
+    def clean(self):
+        if self.date_renewal < date.today():
+            raise ValidationError({"date_renewal": ValidationError("Renewal date cannot be in the past.")})
+        if self.date_created:
+            if self.date_renewal <= self.date_created:
+                raise ValidationError({"date_renewal": ValidationError("Renewal date must be after created date.")})
