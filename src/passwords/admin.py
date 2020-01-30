1191cf9719aceeb8ae1a0d27cc88c5b88ac9cf3c
Gabriel Ionescu <ionescuig@yahoo.com>
2020-01-30 22:41:46 +0000

    deleted app: passwords

--- src/passwords/admin.py
+++
@@ -1,9 +0,0 @@
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !!! No need to be added in admin !!!
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-from django.contrib import admin
-from .models import Password
-
-
-admin.site.register(Password)
--- src/passwords/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Password(models.Model):
-    username = models.CharField(max_length=25)
-    password = models.CharField(max_length=50)
