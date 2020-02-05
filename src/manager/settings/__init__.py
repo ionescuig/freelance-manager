a1baade0086c8804cf0c4154745b31594fde5a38
Gabriel Ionescu <ionescuig@yahoo.com>
2020-02-05 19:03:59 +0000

    removed print messages

--- src/manager/settings/__init__.py
+++ src/manager/settings/__init__.py
@@ -2,8 +2,14 @@ from .base import *
 
 try:
     from .local import *
-    print('>>> Loaded local settings')
 except Exception as e:
-    print('>>> Error in local settings:', e)
     from .production import *
-    print('>>> Loaded production settings')
+
+# # for testing only
+# try:
+#     from .local import *
+#     print('>>> Loaded local settings')
+# except Exception as e:
+#     print('>>> Error in local settings:', e)
+#     from .production import *
+#     print('>>> Loaded production settings')
