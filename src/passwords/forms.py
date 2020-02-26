74719efea663273e0fe39c9b7d8547376bed8da0
Gabriel Ionescu <ionescuig@yahoo.com>
2020-02-26 17:14:39 +0000

    Front end for passwords: list and detail only

--- src/passwords/forms.py
+++ src/passwords/forms.py
@@ -7,3 +7,8 @@ class PasswordForm(forms.ModelForm):
     class Meta:
         model = Password
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(PasswordForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control form-control-sm'
